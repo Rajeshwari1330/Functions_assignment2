b1cdc74c81eba2a53e30f320e1420371ecd18cb7
Rajeshwari1330 <yallalraj1330@gmail.com>
2020-06-05 18:55:17 +0530

    Prints single digit  number in words

---
+++ Number_In_Word.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -x
+read -p "Enter a single digit number :" number
+
+function NumberInWords() {
+case $number in
+0)
+        echo "Zero"
+        ;;
+1)
+        echo "One"
+        ;;
+2)
+        echo "Two"
+        ;;
+3)
+        echo "Three"
+        ;;
+4)
+        echo "Four"
+        ;;
+5)
+        echo "Five"
+        ;;
+6)
+        echo "Six"
+        ;;
+7)
+        echo "Seven"
+        ;;
+8)
+        echo "Eight"
+        ;;
+9)
+        echo "Nine"
+        ;;
+*)
+        echo "Please enter a correct input number"
+        ;;
+esac
+}
+NumberInWords
+
+
